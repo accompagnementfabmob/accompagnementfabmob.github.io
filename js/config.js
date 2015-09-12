e94be9196adf450e8f17be88fe6441d0e0a70c4e
SimonSarazin <simon.sarazin@gmail.com>
2015-09-12 11:52:35 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
     defaultResultLimit: 18,
     bucket_uri: 'http://data.patapouf.org/bucket/upload/',
     loginBaseUrl: 'http://data.patapouf.org/api/v0',
-    oauthBaseUrl: 'http://accompagnementfabmob.github.io',
+    oauthBaseUrl: 'http://institutprospective.github.io',
     oauthCliendId: '834927693290-fdibik9cv2ngiqpdn47qjlc1tqp97cl2.apps.googleusercontent.com',
     media_uri: 'http://data.patapouf.org',
     rest_uri: "http://data.patapouf.org/api/v0",
